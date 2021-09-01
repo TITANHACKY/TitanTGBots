919fe7ae0de72e51fc5ebaeaf8ad8b7b9d6459f3
poonkawin <poonkawin8484@gmail.com>
2021-09-01 12:01:55 +0530

    added Top Button

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function Toogle(id) {
     y.style.display = "block";
   }
 }
+
+
+var topbutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
